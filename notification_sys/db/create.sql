b96411fbaf4b4db6515b9ae2775006da6049b7d5
seemycode <murakami.lelis@gmail.com>
2021-11-18 02:16:06 -0300

    notification table

--- notification_sys/db/create.sql
+++ notification_sys/db/create.sql
@@ -17,6 +17,28 @@ COMMENT ON COLUMN client_schema.user_device.fcm_token IS 'FCM Registration Token
 COMMENT ON COLUMN client_schema.user_device.platform IS 'It can be android, ios, or web';
 COMMENT ON COLUMN client_schema.user_device.updated_at IS 'Used to clean up staled tokens';
 
+CREATE TABLE client_schema.notification (
+	id int NOT NULL GENERATED ALWAYS AS IDENTITY,
+	title varchar NOT NULL,	
+	message varchar NOT NULL,	
+	sender_id varchar NOT NULL,
+	recipient_id varchar NOT NULL,
+	created_at timestamp without time zone NULL DEFAULT  (now() at time zone 'utc'),
+	is_read boolean default false,
+	read_at timestamp without time zone NULL,
+	CONSTRAINT notification_pk PRIMARY KEY (id)
+);
+
+COMMENT ON TABLE client_schema.notification IS 'Holds notification data';
+COMMENT ON COLUMN client_schema.notification.id IS 'Notification ID';
+COMMENT ON COLUMN client_schema.notification.title IS 'Notification title';
+COMMENT ON COLUMN client_schema.notification.message IS 'Notification message';
+COMMENT ON COLUMN client_schema.notification.sender_id IS 'Who sent the notification';
+COMMENT ON COLUMN client_schema.notification.recipient_id IS 'Who received the notification';
+COMMENT ON COLUMN client_schema.notification.created_at IS 'When the notification was created at';
+COMMENT ON COLUMN client_schema.notification.is_read IS 'Whether the notification was read or not';
+COMMENT ON COLUMN client_schema.notification.read_at IS 'When the notification was read at';
+
 /* run clients this user instead */
 CREATE USER notification_grpc_user WITH PASSWORD '<<USER_PASSWORD>>';
 
@@ -27,3 +49,4 @@ grant all privileges on database notification_grpc_db to notification_grpc_user;
 grant execute on all functions in schema client_schema to notification_grpc_user;
 grant usage on schema client_schema to notification_grpc_user;
 grant all on all tables in schema client_schema to notification_grpc_user;
+
